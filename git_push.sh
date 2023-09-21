aa0515e147ef360c1f8ac00fc842da915ff18c3f
pnjrmdn <panjiramadanu55@gmail.com>
2023-09-21 18:49:01 +0000

    .

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 date '+%Y-%m-%d %H:%M:%S' > /home/github_cron/date.txt
-git add .
-git commit -m "."
-git push origin main
+/usr/bin/git add .
+/usr/bin/git commit -m "."
+/usr/bin/git push origin main
